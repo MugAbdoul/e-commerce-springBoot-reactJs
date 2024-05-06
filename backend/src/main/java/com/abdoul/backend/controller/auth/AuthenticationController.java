1d40d8642ee0ad82bca48ad9f800a6618347de12
MugAbdoul <abdoullatif5027@gmail.com>
2024-05-06 21:43:53 +0200

    Cart Feature Added

--- backend/src/main/java/com/abdoul/backend/controller/auth/AuthenticationController.java
+++ backend/src/main/java/com/abdoul/backend/controller/auth/AuthenticationController.java
@@ -1,4 +1,4 @@
-package com.abdoul.backend.controller;
+package com.abdoul.backend.controller.auth;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
